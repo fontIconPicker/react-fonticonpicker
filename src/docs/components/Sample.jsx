fcd40d170e74447931bf5ced5b98deb55c3aef98
Swashata Ghosh <swashata@wpquark.com>
2018-04-06 19:17:01 +0530

    docs: update readme

---
+++ src/docs/components/Sample.jsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2018 Swashata Ghosh <swashata@wpquark.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+import React from 'react';
+import {render} from 'react-dom';
+import
+class App
