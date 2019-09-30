cfc5e768914636c40ab5a924a18e6ad9b754b2da
Greg <greg@myschoolportal.co.uk>
2019-09-30 15:38:56 +0100

    Update FontIconPicker.jsx

--- src/js/components/FontIconPicker.jsx
+++ src/js/components/FontIconPicker.jsx
@@ -225,7 +225,7 @@ class FontIconPicker extends React.PureComponent {
 			this.closeDropdown();
 		}
 	};
-	isClickWithin = target => {
+	isClickWithin = /* istanbul ignore next */  target => {
 		if (
 			target.className === 'fipicon-angle-left' ||
 			target.className === 'fipicon-angle-right' ||
