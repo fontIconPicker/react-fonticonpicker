a2413e8d80d4632d0da89bb1f46d3cd329cdddc0
Kai Felix Missalla <kaifelix.missalla@eu.panasonic.com>
2020-09-01 16:36:38 +0200

    Updated dist and package json's

---
+++ dist/base-theme.react.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * React FontIconPicker
+ * 
+ * React Component to show a picker element to pick font-icons & svg
+ * 
+ * @author Swashata Ghosh <swashata@wpquark.com>
+ * @version 1.2.0
+ * @link https://github.com/fontIconPicker/react-fonticonpicker
+ * @license MIT
+ * 
+ * Copyright (c) 2020 Swashata Ghosh <swashata@wpquark.com>
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ * 
+ */
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.FontIconPicker=t():e.FontIconPicker=t()}(window,function(){return function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}return n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:o})},n.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=5)}({47:function(e,t){},5:function(e,t,n){"use strict";n(56),n(47)},56:function(e,t){}}).default});
+//# sourceMappingURL=base-theme.react.js.map
---
+++ dist/material-theme.react.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * React FontIconPicker
+ * 
+ * React Component to show a picker element to pick font-icons & svg
+ * 
+ * @author Swashata Ghosh <swashata@wpquark.com>
+ * @version 1.2.0
+ * @link https://github.com/fontIconPicker/react-fonticonpicker
+ * @license MIT
+ * 
+ * Copyright (c) 2020 Swashata Ghosh <swashata@wpquark.com>
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ * 
+ */
+!function(n,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.FontIconPicker=t():n.FontIconPicker=t()}(window,function(){return function(n){var t={};function o(e){if(t[e])return t[e].exports;var u=t[e]={i:e,l:!1,exports:{}};return n[e].call(u.exports,u,u.exports,o),u.l=!0,u.exports}return o.m=n,o.c=t,o.d=function(n,t,e){o.o(n,t)||Object.defineProperty(n,t,{configurable:!1,enumerable:!0,get:e})},o.r=function(n){Object.defineProperty(n,"__esModule",{value:!0})},o.n=function(n){var t=n&&n.__esModule?function(){return n.default}:function(){return n};return o.d(t,"a",t),t},o.o=function(n,t){return Object.prototype.hasOwnProperty.call(n,t)},o.p="",o(o.s=4)}([,,,,function(n,t,o){"use strict";o(45),o(43),o(41),o(39),o(37),o(35),o(33),o(31),o(29),o(27),o(25),o(23),o(21),o(19),o(17)},,,,,,,,,,,,,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){},,function(n,t){}]).default});
+//# sourceMappingURL=material-theme.react.js.map
